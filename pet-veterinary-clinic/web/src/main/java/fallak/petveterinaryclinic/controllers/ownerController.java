85645bca1b20b156f079412003adcdd491575241
fallak <sh.fallak@gmail.com>
2020-06-09 23:57:52 +0200

    add Owner index page and controller.closes #13

---
+++ pet-veterinary-clinic/web/src/main/java/fallak/petveterinaryclinic/controllers/ownerController.java
@@ -0,0 +1,13 @@
+package fallak.petveterinaryclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping({"/owners"})
+@Controller
+public class ownerController {
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
+
