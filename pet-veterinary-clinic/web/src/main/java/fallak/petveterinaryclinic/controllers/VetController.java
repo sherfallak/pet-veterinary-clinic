7cb12aa99178554a48af85b71a3dbd59c4cc56a2
fallak <sh.fallak@gmail.com>
2020-06-20 07:11:24 +0200

    add bootstrap for data.closes #14

--- pet-veterinary-clinic/web/src/main/java/fallak/petveterinaryclinic/controllers/VetController.java
+++ pet-veterinary-clinic/web/src/main/java/fallak/petveterinaryclinic/controllers/VetController.java
@@ -2,11 +2,13 @@ package fallak.petveterinaryclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping({"/vets"})
 @Controller
 public class VetController {
 
-    @RequestMapping({"vets","/vets","vets/index","vets/index.html"})
-    public String listOfVets(){
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listVets(){
         return "vets/index";
     }
 }
