7aa53c1ffd96d6d0a16de2e238291a6578f2547e
fallak <sh.fallak@gmail.com>
2020-06-20 17:38:47 +0200

    add vets list and controller. closes #15

--- pet-veterinary-clinic/web/src/main/java/fallak/petveterinaryclinic/bootstrap/DataLoader.java
+++ pet-veterinary-clinic/web/src/main/java/fallak/petveterinaryclinic/bootstrap/DataLoader.java
@@ -40,17 +40,23 @@ private final VetService vetService;
 
         ownerService.save(owner2);
 
+        Owner owner3=new Owner();
+        owner1.setId(3L);
+        owner1.setFirstName("sher");
+        owner1.setLastName("fallak");
+
+        ownerService.save(owner3);
         System.out.println("owner loaded...");
 
         Vet vet1=new Vet();
-        vet1.setId(3L);
+        vet1.setId(1L);
         vet1.setFirstName("Dr. ali");
         vet1.setLastName("khan");
 
         vetService.save(vet1);
 
         Vet vet2=new Vet();
-        vet1.setId(4L);
+        vet1.setId(2L);
         vet1.setFirstName("Dr. sheraz");
         vet1.setLastName("anjum");
         vetService.save(vet2);
