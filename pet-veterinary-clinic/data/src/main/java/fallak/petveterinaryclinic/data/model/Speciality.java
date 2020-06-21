1825f8680d3d3e5ad9964a29818213d3e4ef2be3
fallak <sh.fallak@gmail.com>
2020-06-21 18:13:58 +0200

    Added Speciality associated to vets. closes #33

---
+++ pet-veterinary-clinic/data/src/main/java/fallak/petveterinaryclinic/data/model/Speciality.java
@@ -0,0 +1,13 @@
+package fallak.petveterinaryclinic.data.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
