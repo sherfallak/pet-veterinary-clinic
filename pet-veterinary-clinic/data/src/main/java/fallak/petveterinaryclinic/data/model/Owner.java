f520c7217c20ec340a15b8564c790bcda98489d1
fallak <sh.fallak@gmail.com>
2020-06-21 22:48:31 +0200

    Added contact info property to Owner. closes #34

--- pet-veterinary-clinic/data/src/main/java/fallak/petveterinaryclinic/data/model/Owner.java
+++ pet-veterinary-clinic/data/src/main/java/fallak/petveterinaryclinic/data/model/Owner.java
@@ -3,13 +3,40 @@ package fallak.petveterinaryclinic.data.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(final String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return this.pets;
     }
 
-    public void setPets(Set<Pet> pets) {
+    public void setPets(final Set<Pet> pets) {
         this.pets = pets;
     }
 }
